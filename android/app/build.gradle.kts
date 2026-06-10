8a16203b080c8b183b246c4df419c14be4ad3c07
Kevin Delfour <kevin@delfour.co>
2026-06-10 21:07:30 +0200

    ci: APK debug arm64 via --split-per-abi (~162 Mo Pixel 7) + nettoyage build.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "co.delfour.kitt"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
         versionCode = flutter.versionCode
